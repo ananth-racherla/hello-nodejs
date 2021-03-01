ce61f20c1442834e7090fd9b38ab04186792337b
Ananth Racherla <ananth.racherla@promethean.tv>
2021-03-01 11:23:52 -0800

    Encrypt mongo data at rest

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,8 @@
 import mongoose from 'mongoose';
+import encrypt  from 'mongoose-encryption';
+
+const encryptionKey = process.env.SOME_32BYTE_BASE64_STRING;
+const signingKey = process.env.SOME_64BYTE_BASE64_STRING;
 
 const UserSchema = new mongoose.Schema({
     firstName: {
@@ -25,5 +29,12 @@ const UserSchema = new mongoose.Schema({
     { timestamps: true },
 );
 
-const User = mongoose.model('User', UserSchema);
+/**
+ * Ensure that the data at rest is encrypted.
+ * Note: fields with indexes are not encrypted by default.
+ * Mongo Enterprise has encryption at rest capabilities which are probably better to switch to.
+ */
+UserSchema.plugin(encrypt, { encryptionKey, signingKey });
+
+export const User = mongoose.model('User', UserSchema);
 export default User;
